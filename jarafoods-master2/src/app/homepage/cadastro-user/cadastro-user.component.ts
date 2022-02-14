0d0bff3e60e695b5f844b2b8dcba3800cb9e486f
Your Name <90930890+SantOtavio@users.noreply.github.com>
2022-02-14 09:42:43 -0300

    a

--- jarafoods-master2/src/app/homepage/cadastro-user/cadastro-user.component.ts
+++ jarafoods-master2/src/app/homepage/cadastro-user/cadastro-user.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AuthService, GoogleLoginProvider } from "angular-6-social-login-v2";
 
 @Component({
-  selector: 'app-cadastro-user',
-  templateUrl: './cadastro-user.component.html',
-  styleUrls: ['./cadastro-user.component.css']
+  selector: "app-cadastro-user",
+  templateUrl: "./cadastro-user.component.html",
+  styleUrls: ["./cadastro-user.component.css"],
 })
 export class CadastroUserComponent implements OnInit {
+  constructor(private socialAuthService: AuthService) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  public socialSignIn() {
+    GoogleLoginProvider.PROVIDER_ID;
+
+    this.socialAuthService
+      .signIn(GoogleLoginProvider.PROVIDER_ID)
+      .then((userData) => {
+        console.log(userData);
+      });
   }
 
+  registerconfirm(){
+    
+  }
 }
