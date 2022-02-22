85f01381f6b31ba4b8167256cbf33b0e97fad9f7
SantOtavio <otavio.santos3006@gmail.com>
2022-02-21 21:35:01 -0300

    a

--- jarafoods-master2/src/app/restcadastro/cadastrorestaurante/cadastrorestaurante.component.ts
+++ jarafoods-master2/src/app/restcadastro/cadastrorestaurante/cadastrorestaurante.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Output } from "@angular/core";
 import { Router } from "@angular/router";
+
+
 
 @Component({
   selector: "app-cadastrorestaurante",
@@ -7,7 +9,29 @@ import { Router } from "@angular/router";
   styleUrls: ["./cadastrorestaurante.component.css"],
 })
 export class CadastrorestauranteComponent implements OnInit {
+
+
+  @Output() newItemEvent = new EventEmitter<string>();
+
+
   constructor(private router: Router) {}
 
   ngOnInit() {}
+
+  imageURL;
+  imgarray = []
+
+  mostrarImagem(event) {
+    const file = new FileReader();
+    file.onload = (e) => {
+      this.imageURL = e.target.result;
+    };
+    file.readAsDataURL(event.target.files[0]);
+  }
+
+
+  confirmIMG() {
+    this.imgarray.push(this.imageURL);
+    console.log(this.imgarray[0])
+  }
 }
